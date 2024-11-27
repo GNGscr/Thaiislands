b5396d6314b9af40d88ef49d93e1fec3af78ffcd
GNGscr <de.brand808@gmail.com>
2024-11-27 23:28:29 +0200

    adding dependency

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -105,7 +105,7 @@ export default function Navbar({
       setIsNavToggleSvgVisible(false);
       setIsNavbarVisible(false);
     };
-  }, [isNavbarVisible]);
+  }, [isNavbarVisible, animate]);
   
   useEffect(() => {
     const intersections =
