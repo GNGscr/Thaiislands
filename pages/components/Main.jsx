b692bbb4764d5db31a7fb6949c698b2ac5b71a60
GNGscr <de.brand808@gmail.com>
2024-11-27 23:36:02 +0200

    remove quotes

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -149,14 +149,14 @@ export default function Main({ data, lang, media, activateMenuIsActive, title, m
           whileInView={{ opacity: 1, y: 0, }}
           transition={{ duration: 0.75, type: "spring" }}
           >
-          <span className="text-7xl text-[#ccc] relative top-[-0.75rem]">"</span>
+          <span className="text-7xl text-[#ccc] relative top-[-0.75rem]"></span>
             {data["article-second-paragraph"][lang]} 
           <div
             className={`blink-emoji text-[2.25rem] absolute bottom-[-0.5rem]
               ${lang === 'he' ? 'left-80' : 'right-60'} pl-9`}>
             😉
           </div>
-          <span className="text-7xl text-[#ccc]" style={{transform: 'rotateZ(180deg)'}}>"</span>
+          <span className="text-7xl text-[#ccc]" style={{transform: 'rotateZ(180deg)'}}></span>
         </motion.div>
       </div>
       <HotelsSection data={data} lang={lang} media={media} />
