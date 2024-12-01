4df1c424d6b25f2198b47bc28c21a582894fae62
GNGscr <de.brand808@gmail.com>
2024-12-01 21:37:20 +0200

    mobile li height

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -369,7 +369,7 @@ const Cursor = ({ position, currentMedia }) => {
       className="absolute z-0 h-7 rounded-full bg-black md:h-12"
       style={
         currentMedia === 'mobile'
-          ? { height: '2.025rem' }
+          ? { height: '2.35rem' }
           : { height: '3rem' }
       }
     />
