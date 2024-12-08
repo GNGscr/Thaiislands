2d874c813d3d53b21a61e979e979e9db43b0cc37
GNGscr <de.brand808@gmail.com>
2024-12-08 19:16:00 +0200

    sticky footer test 2

--- pages/components/StickyFooter.jsx
+++ pages/components/StickyFooter.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+"use client";
 import Noise from './Noise';
-import { motion } from 'framer-motion';
+import React, { useRef, useEffect } from "react";
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 // [#c8bdb0]
 export default function StickyFooter({ data, lang }) {
+    const container = useRef();
+    const { scrollYProgress } = useScroll({
+      target: container,
+      offset: ['start start', 'end start']
+    });
+      // Set the color to switch by toggle (ease fnc) between sections
+  const isDisplayed = useTransform(
+    scrollYProgress,
+    [0, 0.15, 0.25, 0.7, 1],
+    ["none", "none", "none", "flex", "flex"],
+    { ease: (t) => Math.round(t) }
+  );
+
+  useEffect(() => {
+  }, [isDisplayed]);
+
     return (
         <motion.div 
             className="footer fixed bottom-0 z-[-1] w-screen gap-4" 
-            style={{ opacity: 0  }}
+            style={{ opacity: 0.85, display: isDisplayed  }}
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 0.85 }}
             transition={{ duration: 0.1 }}
