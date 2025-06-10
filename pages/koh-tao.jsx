5b70e196f7869c9c2886f91edd36e415498a6ed4
GNGscr <de.brand808@gmail.com>
2025-06-10 19:52:53 +0300

    fix temp for move to db 1

--- pages/koh-tao.jsx
+++ pages/koh-tao.jsx
@@ -6,41 +6,41 @@ import IslandPageLayout from "./layouts/IslandPageLayout";
 import { islandIdMap } from "@/lib/constants/privateData";
 import SectionAnimation from "./components/SectionAnimation";
 import { LANG } from "./public/data/en.json";
-import kohTaoData from "./public/data/kohTaoData.json";
+// import kohTaoData from "./public/data/kohTaoData.json";
 
 export default function KohTao() {
   const { currentMedia } = useGlobalSettings();
-  // const [data, setData] = useState();
-  // const islandId = islandIdMap.kohTaoDataId;
+  const [data, setData] = useState();
+  const islandId = islandIdMap.kohTaoDataId;
 
-  // const [isLoading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  //   useEffect(() => {
-  //     fetchIsland();
-  //   }, []);
+    useEffect(() => {
+      fetchIsland();
+    }, []);
 
-  //   const fetchIsland = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const res = await fetch('/api/islands?' + new URLSearchParams({ id: islandId }).toString());
-  //       if (!res.ok) throw new Error("Failed to fetch island data");
-  //       const json = await res.json();
-  //       setData(json);
-  //     } catch (err) {
-  //       setError(err.message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
+    const fetchIsland = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch('/api/islands?' + new URLSearchParams({ id: islandId }).toString());
+        if (!res.ok) throw new Error("Failed to fetch island data");
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
     
-  //   if (isLoading) return <SectionAnimation menuIsActive={isLoading} title={LANG.KOH_TAO} />;
-  //   if (error) return <p>שגיאה: {error}</p>;
-  //   if (!data) return;
+    if (isLoading) return <SectionAnimation menuIsActive={isLoading} title={LANG.KOH_TAO} />;
+    if (error) return <p>שגיאה: {error}</p>;
+    if (!data) return;
 
     return (
       <IslandPageLayout
-        data={kohTaoData}
+        data={data}
         media={currentMedia}
         mapImage={kohTaoMap.src}
       />
