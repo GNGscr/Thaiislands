9ec943a1768213aa103b8c93fe7bb5f3d96f23ed
GNGscr <de.brand808@gmail.com>
2025-06-11 20:37:33 +0300

    add meta data to head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Thaiislands</title>
+        <meta name="description" content="גלו את קו פנגן - האי המושלם לחופים, מסיבות ותרבות בתאילנד. כל מה שצריך לדעת על טיול לאי." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
