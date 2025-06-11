c13651e27b2757d39043ccfcce03ec79b303c78b
GNGscr <de.brand808@gmail.com>
2025-06-11 20:51:23 +0300

    fix map bug 2

--- pages/components/MapEmbed.jsx
+++ pages/components/MapEmbed.jsx
@@ -8,18 +8,19 @@ export default function MapEmbed({ link, lang }) {
     setIsClient(true);
   }, []);
 
-  if (!isClient || !link || !lang) return null;
+  if (!isClient || !link) return null;
 
   return (
     <div className="map-responsive">
-        <iframe src={data.googleMap.link}
-        width={lang === "Koh Phangan" ? "600" : "100%"}
-        height="450"
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Responsive Google Map">
-        </iframe>
+        <iframe
+            src={link}
+            width={lang === "Koh Phangan" ? "600" : "100%"}
+            height="450"
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Responsive Google Map"
+        ></iframe>
     </div>
   );
 }
