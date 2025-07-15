5b4b7ae2a9fcb8f27a4536887aeeb1116d8152ee
GNGscr <de.brand808@gmail.com>
2025-07-15 11:00:37 +0300

    add 404 page

---
+++ src/components/common/DolphinAnimation.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useRive, useStateMachineInput } from 'rive-react';
+
+const STATE_MACHINE_NAME = "State Machine 1";
+
+const DolphinAnimation = ({ menuIsActive, media }) => {
+
+    const {rive, RiveComponent} = useRive({
+        src: "dolphin.riv",
+        autoplay: true,
+        stateMachines: STATE_MACHINE_NAME
+    })
+
+    const stateSuccess = useStateMachineInput(
+        rive,
+        STATE_MACHINE_NAME,
+        'Timeline 1'
+    )
+    const triggerSuccess = () => {
+        stateSuccess && stateSuccess.fire();
+    }
+    
+    if (rive) {
+      triggerSuccess()
+    }
+    
+    return (
+        <motion.div
+        style={{borderRadius: '50%'}}
+            className="slide-in-rive"
+            initial={{ opacity: 0 }}
+            animate={ menuIsActive ? { opacity: 1 } : { opacity: 0 } }
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+        >
+            <motion.div
+                className={`flex justify-center align-center`}
+                style={{
+                    padding: "0",
+                    borderRadius: "50%",
+                    background: "#000"
+                }}>
+                    <RiveComponent
+                        style={{
+                            width: media === 'mobile' ? '300px' : '400px',
+                            height: media === 'mobile' ? '300px' : '400px',
+                            borderRadius: "50%",
+                            marginTop: "0rem",
+                            clipPath: 'circle(40%)'
+                        }}
+                        src="dolphin.riv"
+                    />
+
+            </motion.div>
+        </motion.div>
+    );
+};
+
+export default DolphinAnimation;
---
+++ src/pages/404.js
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import Head from "next/head";
+import DolphinAnimation from "../components/common/DolphinAnimation";
+import { motion } from "framer-motion";
+
+export default function Custom404() {
+    const spacingIndex = {
+        The: 4,
+        page: 8,
+        "you're": 16,
+        looking: 24,
+        for: 28,
+        "doesn't": 36,
+        "exist.": 42,
+      };
+      const spacingIndices = Object.values(spacingIndex);
+      const sentence = "The page you're looking for doesn't exist.";
+const letters = sentence.split("");
+  return (
+    <>
+      <Head>
+        <title>404 – Page Not Found</title>
+      </Head>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-[#121212] text-white px-4 text-center">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <div className="404-dolphin-animation">
+          <DolphinAnimation menuIsActive={true} media={"mobile"} />
+        </div>
+        <div className="text-2xl mb-6">
+          <div className="w-full flex align-center justify-center text-[1.5rem] mb-[5px]">
+            {letters.map((char, i) => {
+              return (
+                <motion.div
+                  key={i}
+                  initial={{ opacity: 0, y: "75%", skewX: 15 }}
+                  whileInView={{ opacity: 1, y: 0, skewX: 0 }}
+                  transition={{
+                    duration: 0.25,
+                    type: "spring",
+                    delay: i * 0.025,
+                  }}
+                >
+                  {char === " " ? "\u00A0" : char}
+                </motion.div>
+              );
+            })}
+          </div>
+        </div>
+
+        <motion.span
+          initial={{ scale: 1 }}
+          animate={{ scale: [1, 1.25, 1, 1.25, 1] }}
+          transition={{
+            delay: 1.35,
+            duration: 0.75,
+            ease: "easeInOut",
+            times: [0, 0.25, 0.5, 0.75, 1],
+          }}
+        >
+          <Link
+            href="/about"
+            className="text-2xl text-blue-300 hover:text-blue-200"
+          >
+            ← Back to Home
+          </Link>
+        </motion.span>
+      </div>
+    </>
+  );
+}
